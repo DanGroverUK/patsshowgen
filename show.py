61bcc67256044d8a59ae2a71adc2ebf383c7257c
Dan Grover <dan-grover@dan-grover.com>
2020-10-03 19:38:36 +0100

    Started on the edit function

--- show.py
+++ show.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 with open('options.json') as fo:
     options = json.load(fo)
-    print(options)
 
 
 def sentenceCapitaliser(para):
@@ -39,6 +38,16 @@ def fillBlanks(para, char1):
                        firstname=pick("firstname"),
                        surname=pick("surname"))
     return desc
+    
+def dictToString(options):
+    newDict = {}
+    for k in options.keys():
+        newstr = ""
+        for o in options[k]:
+            newstr = newstr + str(o) + "\n"
+        newDict[k] = newstr
+    return newDict
+
 
 
 @app.route('/')
@@ -48,7 +57,25 @@ def gen_show():
     para = pick("para")
     desc = fillBlanks(para, char1)
     return render_template("index.html", title=title.upper(), desc=sentenceCapitaliser(desc))
-
+    
+@app.route('/edit')
+def edit_show():
+    opts = dictToString(options)
+    return render_template("edit.html",
+        title=opts["title"],
+        place=opts["place"],
+        char1=opts["char1"],
+        char2=opts["char2"],
+        char2_possession=opts["char2_possession"],
+        relationship=opts["relationship"],
+        shared_interest=opts["shared_interest"],
+        mission=opts["mission"],
+        firstname=opts["firstname"],
+        surname=opts["surname"],
+        historic_element=opts["historic_element"],
+        historic_where=opts["historic_where"],
+        action=opts["action"],
+        final=opts["final"])
 
 if __name__ == "__main__":
     app.run()
